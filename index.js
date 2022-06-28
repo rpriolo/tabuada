f3744e8aaea825a2dd729c23ad34c5663a04de9f
Renato Priolo <rpriolo@gmail.com>
2022-06-28 10:47:56 -0300

    Adição de condição lógica

--- index.js
+++ index.js
@@ -5,28 +5,29 @@ function gerarTabuada() {
     var table = document.querySelector('table#resultado');
     table.innerHTML = ''; // Limpa o resultado anterior
 
-    var inputNumero = document.querySelector('input#numero');
-    var numeroDigitado = Number(inputNumero.value);
+    var numeroDigitado = document.querySelector('input#numero').value;
 
+    if (numeroDigitado.length == 0) {
+        alert('Digite um número antes de continuar.');
+    } else {
+        for (var i = 0; i <= 10; i++) {
+            var tr = document.createElement('tr');
+            table.appendChild(tr);
+            var td1 = document.createElement('td');
+            td1.setAttribute('id', 'conta')
+            var td2 = document.createElement('td');
+            td2.setAttribute('id', 'igual')
+            var td3 = document.createElement('td');
+            td3.setAttribute('id', 'resultado')
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
     
-    for (var i = 0; i <= 10; i++) {
-        var tr = document.createElement('tr');
-        table.appendChild(tr);
-        var td1 = document.createElement('td');
-        td1.setAttribute('id', 'conta')
-        var td2 = document.createElement('td');
-        td2.setAttribute('id', 'igual')
-        var td3 = document.createElement('td');
-        td3.setAttribute('id', 'resultado')
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-        tr.appendChild(td3);
-
-        var resultadoTabuada = numeroDigitado * i;
-
-        td1.innerHTML = `${numeroDigitado} × ${i}`;
-        td2.innerHTML = `=`;
-        td3.innerHTML = `${resultadoTabuada}`;
-
+            var resultadoTabuada = numeroDigitado * i;
+    
+            td1.innerHTML = `${numeroDigitado} × ${i}`;
+            td2.innerHTML = `=`;
+            td3.innerHTML = `${resultadoTabuada}`;
+        }
     }
 };
